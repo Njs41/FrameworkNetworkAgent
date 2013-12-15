349c23869a043219d6a4cf03926c47147eddf41c
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:37:59 +0100

    Declare features used

--- src/no/runsafe/frameworknetworkagent/Plugin.java
+++ src/no/runsafe/frameworknetworkagent/Plugin.java
@@ -1,12 +1,14 @@
 package no.runsafe.frameworknetworkagent;
 
 import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		addComponent(Events.class);
 		addComponent(AgentHook.class);
 	}
 }
