6d572e9e95e618610c3f2d48a9553669c8c08b37
Morten Nilsen <morten@runsafe.no>
2013-12-15 14:00:43 +0100

    PluginEnabled is not driven by EventEngine

--- src/no/runsafe/frameworknetworkagent/Plugin.java
+++ src/no/runsafe/frameworknetworkagent/Plugin.java
@@ -1,14 +1,12 @@
 package no.runsafe.frameworknetworkagent;
 
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
-		addComponent(Events.class);
 		addComponent(AgentHook.class);
 	}
 }
